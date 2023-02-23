6578643dd14d74b0705f44342786dfed764fd0eb
mubai1030 <mubai1030@gmail.com>
2023-02-23 17:09:27 +0800

    02231706

---
+++ src/main/java/com/by/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.by.git;
+
+/**
+ * @author xiaobai
+ * @create 2023-02-23 16:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}
